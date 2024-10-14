f2458b41ddb6c2ecc146b77850485606292e40c1
Rd-1705 <akbarhidayat0812@gmail.com>
2024-10-14 20:40:57 +0700

    Add project files.

---
+++ pertemuan10/Model/Connection.cs
@@ -0,0 +1,36 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pertemuan10.Model
+{
+    internal class Connection
+    {
+        public MySqlCommand cmd;
+        public DataSet ds;
+        public MySqlDataAdapter ad;
+        public MySqlConnection conn;
+
+        public MySqlConnection getcon()
+        {
+            conn = new MySqlConnection();
+            conn.ConnectionString = "server=localhost;user=root;database=db_contoh";
+
+            try
+            {
+                conn.Open();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi gagal " + ex.Message);
+            }
+            return conn;
+        }
+    }
+}
